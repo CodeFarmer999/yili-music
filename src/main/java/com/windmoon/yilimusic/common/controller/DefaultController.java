c7c8cbed38c08497b6f9ff85f9c434d6ec95aa81
Shuoshuo <liqiangge628@gmail.com>
2022-03-16 10:42:41 +0800

    update pom

--- src/main/java/com/windmoon/yilimusic/common/controller/DefaultController.java
+++ src/main/java/com/windmoon/yilimusic/common/controller/DefaultController.java
@@ -1,4 +1,4 @@
-package com.windmoon.yilimusic.controller;
+package com.windmoon.yilimusic.common.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
